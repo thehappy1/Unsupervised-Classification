81cae4d592526f9cc119c8439eca2bba62a35915
Felix <Felix.Schmedes@uni-oldenburg.de>
2021-01-17 15:42:27 +0100

    removed targets = batch['target'].cuda(non_blocking=True). It caused an error

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -26,7 +26,7 @@ def simclr_train(train_loader, model, criterion, optimizer, epoch):
         input_ = torch.cat([images.unsqueeze(1), images_augmented.unsqueeze(1)], dim=1)
         input_ = input_.view(-1, c, h, w) 
         input_ = input_.cuda(non_blocking=True)
-        targets = batch['target'].cuda(non_blocking=True)
+        #targets = batch['target'].cuda(non_blocking=True)
 
         output = model(input_).view(b, 2, -1)
         loss = criterion(output)
