87a436e938427d038084ec255a3d957348fa2447
Felix <Felix.Schmedes@uni-oldenburg.de>
2021-02-25 10:05:19 +0100

    fixed calc_mean

--- data/calc_mean.py
+++ data/calc_mean.py
@@ -10,5 +10,5 @@ dataloader = DataLoader(dataset, batch_size=len(dataset), num_workers=1)
 data = next(iter(dataloader))
 
 
-data[0].mean(), data[0].std()
+print("mean: ", data[0].mean(), "std: ",data[0].std())
 
