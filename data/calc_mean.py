a559a7261ab689a72ab592234eebe785c596a151
Felix <Felix.Schmedes@uni-oldenburg.de>
2021-02-25 10:20:44 +0100

    testing

--- data/calc_mean.py
+++ data/calc_mean.py
@@ -1,14 +1,18 @@
 from fpidataset import Fpidataset
 from torch.utils.data import DataLoader
+from fashion import Fashion
 
-dataset = Fpidataset(train=True, img_size=60, transform=None)
-
-
-dataloader = DataLoader(dataset, batch_size=len(dataset), num_workers=1)
+#dataset = Fpidataset(train=True, img_size=60, transform=None)
 
 
-data = next(iter(dataloader))
+#dataloader = DataLoader(dataset, batch_size=len(dataset), num_workers=1)
 
 
-print("mean: ", data[0].mean(), "std: ",data[0].std())
+#data = next(iter(dataloader))
 
+
+#print("mean: ", data[0].mean(), "std: ",data[0].std())
+
+dataset2 = Fashion(train=True, transform=None)
+
+print(dataset2.df)
