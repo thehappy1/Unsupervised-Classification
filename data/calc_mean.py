42b54797856cf928c6cf29db88c1fed066432507
Felix <Felix.Schmedes@uni-oldenburg.de>
2021-02-25 10:04:17 +0100

    fixed calc_mean

--- data/calc_mean.py
+++ data/calc_mean.py
@@ -3,8 +3,12 @@ from torch.utils.data import DataLoader
 
 dataset = Fpidataset(train=True, img_size=60, transform=None)
 
+
 dataloader = DataLoader(dataset, batch_size=len(dataset), num_workers=1)
+
 
 data = next(iter(dataloader))
 
+
 data[0].mean(), data[0].std()
+
