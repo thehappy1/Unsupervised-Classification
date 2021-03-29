a7aa77d3e3f97bc2a5940a3bd294e31f5179d09c
Felix <Felix.Schmedes@uni-oldenburg.de>
2021-03-29 12:25:10 +0200

    validation

--- selflabel.py
+++ selflabel.py
@@ -113,7 +113,7 @@ def main():
     # Evaluate and save the final model
     print(colored('Evaluate model at the end', 'blue'))
 
-    predictions = get_predictions(p, val_dataloader, model, return_features=True)
+    predictions = get_predictions(p, val_dataloader, model)
     clustering_stats = hungarian_evaluate(0, predictions, 
                                 class_names=val_dataset.classes,
                                 compute_confusion_matrix=True,
