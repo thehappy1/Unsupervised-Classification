047e9dc49ca55340233c02a171a15e4950741ef6
Felix <Felix.Schmedes@uni-oldenburg.de>
2021-04-09 13:40:26 +0200

    fixed the changed true/false parameter of train-test loading datasets

--- data/custom_dataset.py
+++ data/custom_dataset.py
@@ -74,6 +74,7 @@ class NeighborsDataset(Dataset):
         neighbor_index = np.random.choice(self.indices[index], 1)[0]
         neighbor = self.dataset.__getitem__(neighbor_index)
 
+        print("achor: ", anchor)
         anchor['image'] = self.anchor_transform(anchor['image'])
         neighbor['image'] = self.neighbor_transform(neighbor['image'])
 
