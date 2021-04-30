7424b8ba4578f02a1bb49a59f6bc09e3173275a0
Felix <Felix.Schmedes@uni-oldenburg.de>
2021-04-30 11:38:23 +0200

    visualization step

--- utils/utils.py
+++ utils/utils.py
@@ -6,6 +6,17 @@ import os
 import torch
 import numpy as np
 import errno
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as PathEffects
+
+import seaborn as sns
+sns.set_style('darkgrid')
+sns.set_palette('muted')
+sns.set_context("notebook", font_scale=1.5,
+                rc={"lines.linewidth": 2.5})
+RS = 123
 
 def mkdir_if_missing(directory):
     if not os.path.exists(directory):
@@ -103,5 +114,33 @@ def compute_tsne(features, labels):
     time_start = time.time()
 
     tsne = TSNE(n_components=2, perplexity=20, n_jobs=16, random_state=0, verbose=0).fit_transform(features)
+
+    # choose a color palette with seaborn.
+    num_classes = len(np.unique(labels))
+    palette = np.array(sns.color_palette("hls", num_classes))
+
+    # create a scatter plot.
+    f = plt.figure(figsize=(8, 8))
+    ax = plt.subplot(aspect='equal')
+    sc = ax.scatter(features[:, 0], features[:, 1], lw=0, s=40, c=palette[labels.astype(np.int)])
+    plt.xlim(-25, 25)
+    plt.ylim(-25, 25)
+    ax.axis('off')
+    ax.axis('tight')
+
+    # add the labels for each digit corresponding to the label
+    txts = []
+
+    for i in range(num_classes):
+        # Position of each label at median of data points.
+
+        xtext, ytext = np.median(features[labels == i, :], axis=0)
+        txt = ax.text(xtext, ytext, str(i), fontsize=24)
+        txt.set_path_effects([
+            PathEffects.Stroke(linewidth=5, foreground="w"),
+            PathEffects.Normal()])
+        txts.append(txt)
+
+    return f, ax, sc, txts
 
     print("t-SNE done! Time elapsed: {} seconds".format(time.time()-time_start))
