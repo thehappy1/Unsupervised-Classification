ac3fe2a7908dda50243538cb84f209acb5024ffe
Felix <Felix.Schmedes@uni-oldenburg.de>
2021-05-03 10:48:38 +0200

    visualization step

--- utils/utils.py
+++ utils/utils.py
@@ -123,7 +123,7 @@ def compute_tsne(features, labels):
     viz_df['label'] = viz_df["label"].map(dict)
     print(viz_df.head(10))
     viz_df.to_csv('tsne.csv')
-    plt.subplots(figsize=(8, 5))
+    plt.subplots(figsize=(15, 8))
     sns.scatterplot(x=0, y=1, hue=viz_df.label.tolist(), legend='full', hue_order=sorted(viz_df['label'].unique()),
                     palette=sns.color_palette("hls", n_colors=10),
                     alpha=.5,
