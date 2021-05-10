82c87d278e4512e1780b52b2d105104a96b7a608
thehappy1 <FelixSchmedes@web.de>
2021-05-10 19:55:20 +0200

    fix

--- utils/evaluate_utils.py
+++ utils/evaluate_utils.py
@@ -144,6 +144,7 @@ def hungarian_evaluate(subhead_index, all_predictions, class_names=None,
     nmi = metrics.normalized_mutual_info_score(targets.cpu().numpy(), predictions.cpu().numpy())
     ari = metrics.adjusted_rand_score(targets.cpu().numpy(), predictions.cpu().numpy())
     print(reordered_preds)
+    print(targets)
     print(int(reordered_preds==targets).sum())
     from s_dbw import S_Dbw
     s_dbw = S_Dbw(features.numpy(), reordered_preds)
